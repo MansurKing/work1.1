519b620cb9c5ba0a396bf8814be7dd2242a6c272
MansurKing <102825225+MansurKing@users.noreply.github.com>
2022-04-05 17:58:14 +0300

    Initial commit

---
+++ Work1.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Work1._1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string day;
+            byte level;
+            int sizeFoot;
+            long customerCount;
+            ulong waitingTimeClient;
+            float playerMoney;
+            double goldCount;
+            bool brainWorks;
+            char greeting = '≠';
+            decimal moneyBoss;
+        }
+    }
+}
